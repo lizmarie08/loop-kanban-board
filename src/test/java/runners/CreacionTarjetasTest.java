54865cb13f83a42f6ab40e23d12ea6c1f7220f43
Lizmarie Feriz <66039930+lizmarie08@users.noreply.github.com>
2023-10-22 23:44:52 -0500

    Agregando creacion de tarjetas

---
+++ src/test/java/runners/CreacionTarjetasTest.java
@@ -0,0 +1,15 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features/creacion_tarjetas.feature", // Ruta a tus archivos de características
+        glue = "stepdefinitions", // Ruta al paquete con las definiciones de pasos
+        plugin = {"pretty", "html:target/cucumber-reports"} // Formato de informes
+        //tags = "@escenario01" // Etiqueta que coincide con el escenario que deseas ejecutar
+)
+public class CreacionTarjetasTest {
+}
